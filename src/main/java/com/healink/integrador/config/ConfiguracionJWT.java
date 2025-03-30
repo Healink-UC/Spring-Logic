22192cae77e1ee5ad65ba51f7ac0dc85826c1d2f
Over.HCV <over.castrillon22396@ucaldas.edu.co>
2025-03-30 12:59:05 -0500

    feat: Auth service restored

--- src/main/java/com/healink/integrador/config/ConfiguracionJWT.java
+++ src/main/java/com/healink/integrador/config/ConfiguracionJWT.java
@@ -3,8 +3,6 @@ package com.healink.integrador.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
 
@@ -12,12 +10,8 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "application.security.jwt")
 @Data
 public class ConfiguracionJWT {
-    @JsonProperty("secret-key")
     private String llaveSecreta;
-
     private long expiracion;
-
-    @JsonProperty("refresh-token")
     private TokenRefresco tokenRefresco;
 
     @Data
@@ -25,7 +19,7 @@ public class ConfiguracionJWT {
         private long expiracion;
     }
 
-    // Método de inicialización para validar la configuración
+    // Inicialización para validar la configuración
     @PostConstruct
     public void init() {
         if (llaveSecreta == null) {
