028379deb4ea7f3371323c10ba2149e936061471
camian <camian6@outlook.com>
2025-04-01 17:31:14 -0500

    Create risk factor arument was changed
    into the method the param received was changed to request body, that is
    not working

--- src/main/java/com/healink/integrador/domain/factor_riesgo/FactorRiesgoController.java
+++ src/main/java/com/healink/integrador/domain/factor_riesgo/FactorRiesgoController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,8 +22,7 @@ public class FactorRiesgoController {
     private FactorRiesgoService factorService;
 
     @PostMapping("/")
-    public ResponseEntity<FactorRiesgo> createFactorRiesgo(@ModelAttribute FactorRiesgoDTO factorDTO) {
-
+    public ResponseEntity<FactorRiesgo> createFactorRiesgo(@RequestBody FactorRiesgoDTO factorDTO) {
         FactorRiesgo factor = factorService.createFactorRiesgo(factorDTO);
         return ResponseEntity.status(201).body(factor);
     }
