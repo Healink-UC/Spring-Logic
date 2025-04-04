a0911326eddbf0397972f86a3f4ca3243aba67c1
Over.HCV <over.castrillon22396@ucaldas.edu.co>
2025-04-04 12:28:08 -0500

    fix: Removing FactorCampaña as it had some erorrs

--- src/main/java/com/healink/integrador/config/ModelMapperConfig.java
+++ src/main/java/com/healink/integrador/config/ModelMapperConfig.java
@@ -1,14 +1,14 @@
-// package com.healink.integrador.config;
+package com.healink.integrador.config;
 
-// import org.modelmapper.ModelMapper;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// @Configuration
-// public class ModelMapperConfig {
+@Configuration
+public class ModelMapperConfig {
 
-//     @Bean
-//     public ModelMapper modelMapper() {
-//         return new ModelMapper();
-//     }
-// }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
