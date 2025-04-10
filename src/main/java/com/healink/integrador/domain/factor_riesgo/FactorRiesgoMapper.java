596d87c42e42df1fe0972a592f94befbd7858da1
JuanDaDiaz1005 <juan.1701920068@ucaldas.edu.co>
2025-04-10 13:09:09 -0500

    fix: se elimina el paquete factor riesgo

--- src/main/java/com/healink/integrador/domain/factor_riesgo/FactorRiesgoMapper.java
+++
@@ -1,8 +0,0 @@
-package com.healink.integrador.domain.factor_riesgo;
-
-import org.mapstruct.Mapper;
-import com.healink.integrador.core.mapper.MapeadorGenerico;
-
-@Mapper(componentModel = "spring")
-public interface FactorRiesgoMapper extends MapeadorGenerico<FactorRiesgo, FactorRiesgoDTO> {
-}
