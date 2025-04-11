25bd2e1c8a00284428ccf6b218bb4877e3dff62f
camian <camian6@outlook.com>
2025-04-11 18:45:35 -0500

    feat:se deja la importacion como estaba

--- src/main/java/com/healink/integrador/domain/atenciones_medicas/AtencionMedicaMapper.java
+++ src/main/java/com/healink/integrador/domain/atenciones_medicas/AtencionMedicaMapper.java
@@ -2,7 +2,7 @@ package com.healink.integrador.domain.atenciones_medicas;
 
 import org.mapstruct.Mapper;
 
-import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.healink.integrador.core.json.JsonMapper;
 import com.healink.integrador.core.mapper.MapeadorGenerico;
 
 @Mapper(componentModel = "spring", uses = { JsonMapper.class })
