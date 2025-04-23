6cc395957b01537f52369f3dcdf64a00810f5f52
camian <camian6@outlook.com>
2025-04-22 20:25:00 -0500

    feat: se eliminaron comentarios innecesarios

--- src/main/java/com/healink/integrador/domain/predicciones/PrediccionesMapper.java
+++ src/main/java/com/healink/integrador/domain/predicciones/PrediccionesMapper.java
@@ -3,7 +3,6 @@ package com.healink.integrador.domain.predicciones;
 import org.mapstruct.Mapper;
 
 import com.healink.integrador.core.json.JsonMapper;
-// import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.healink.integrador.core.mapper.MapeadorGenerico;
 
 @Mapper(componentModel = "spring", uses = { JsonMapper.class })
