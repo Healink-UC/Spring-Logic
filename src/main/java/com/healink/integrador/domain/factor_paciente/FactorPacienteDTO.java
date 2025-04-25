67f2371f9cd0ce8139f1c1c7f990fab202257ed8
camian <camian6@outlook.com>
2025-04-25 17:54:15 -0500

    feat:tipos de mensajes cambiados en el dto

--- src/main/java/com/healink/integrador/domain/factor_paciente/FactorPacienteDTO.java
+++ src/main/java/com/healink/integrador/domain/factor_paciente/FactorPacienteDTO.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import com.healink.integrador.core.dto.DTOBase;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -12,18 +13,18 @@ public class FactorPacienteDTO implements DTOBase {
 
     private Long id;
 
-    @NotBlank(message = "El paciente id es requerido")
+    @NotNull(message = "El paciente id es requerido")
     private Long pacienteId;
 
-    @NotBlank(message = "El factor de riesgo id es requerido")
+    @NotNull(message = "El factor de riesgo id es requerido")
     private Long factorRiesgoId;
 
-    @NotBlank(message = "El triaje id es requerido")
+    @NotNull(message = "El triaje id es requerido")
     private Long triajeId;
 
-    // @NotBlank(message = "El nombre de factor de riesgo es requerido")
-    // private String valor; //TODO: nombre muy generico, verificar
+    @NotBlank(message = "El nombre de factor de riesgo es requerido")
+    private String valor; // TODO: nombre muy generico, verificar
 
-    @NotBlank(message = "La fecha de registro es requerida")
+    @NotNull(message = "La fecha de registro es requerida")
     private LocalDate fecha_registro;
 }
