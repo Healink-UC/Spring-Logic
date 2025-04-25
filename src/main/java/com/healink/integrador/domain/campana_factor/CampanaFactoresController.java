60f08dfc4062bb7524c9588b9c8c56562a145df2
camian <camian6@outlook.com>
2025-04-25 12:37:22 -0500

    feat:se agregó el tag de la ruta
    se agrego el nombre el tag para las rutas de factores de riesgo

--- src/main/java/com/healink/integrador/domain/campana_factor/CampanaFactoresController.java
+++ src/main/java/com/healink/integrador/domain/campana_factor/CampanaFactoresController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.healink.integrador.core.controller.ControladorGenerico;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/campana-factores")
+@Tag(name = "Campaña factores")
 public class CampanaFactoresController extends ControladorGenerico<CampanaFactores, CampanaFactoresDTO> {
 
     public CampanaFactoresController(CampanaFactoresService campanaFactoresService,
