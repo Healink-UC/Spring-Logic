5ddbaaa90e0bd55258b40f43a9471d4bfa2b9c83
Julian Rivera <jrivera@consensussa.com>
2025-04-25 08:27:18 -0500

    feat: Ajustes mensajes de error DTO

--- src/main/java/com/healink/integrador/domain/citaciones_medicas/CitacionMedicaDTO.java
+++ src/main/java/com/healink/integrador/domain/citaciones_medicas/CitacionMedicaDTO.java
@@ -26,17 +26,24 @@ public class CitacionMedicaDTO implements DTOBase {
     @NotNull(message = "El ID del medico es requerido")
     private Long medicoId;
 
+    @NotNull(message = "El campo hora programada es requerido")
     private LocalDateTime horaProgramada;
 
+    @NotNull(message = "El campo hora atención es requerido")
     private LocalDateTime horaAtencion;
 
+    @NotNull(message = "El campo duración estimada es requerido")
     private Integer duracionEstimada;  // en minutos
 
+    @NotNull(message = "El campo estado es requerido")
     private String estado;
 
+    @NotNull(message = "El campo predicción asistencia es requerido")
     private BigDecimal prediccionAsistencia;  // 0-100%
 
+    @NotNull(message = "El campo prioridad es requerido")
     private Integer prioridad;  // 1-5
 
+    @NotNull(message = "El campo notas es requerido")
     private String notas;
 }
