e93bdc0290d59b074ab100e423c7091ecfca9db8
camian <camian6@outlook.com>
2025-05-05 15:27:51 -0500

    feat: se agrega configuracion previa para el test
    se hacen las configuraciones previas para probar si en el test el
    guardado correctamente de la campaña

--- src/test/java/com/healink/integrador/campana/CampanaTest.java
+++ src/test/java/com/healink/integrador/campana/CampanaTest.java
@@ -1,36 +1,78 @@
 package com.healink.integrador.campana;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
-import com.healink.integrador.domain.campana.Campana;
-import com.healink.integrador.domain.campana.CampanaRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.healink.integrador.domain.campana.CampanaController;
+import com.healink.integrador.domain.campana.CampanaDTO;
+import com.healink.integrador.domain.campana.EstadoCampana;
+import com.healink.integrador.domain.entidades_salud.EntidadSalud;
+import com.healink.integrador.domain.entidades_salud.EntidadSaludRepository;
+import com.healink.integrador.domain.entidades_salud.Estado;
+import com.healink.integrador.domain.localizacion.Localizacion;
+import com.healink.integrador.domain.localizacion.LocalizacionRepository;
+import com.healink.integrador.domain.rol.Rol;
+import com.healink.integrador.domain.rol.RolRepository;
+import com.healink.integrador.domain.usuario.TipoIdentificacion;
+import com.healink.integrador.domain.usuario.Usuario;
+import com.healink.integrador.domain.usuario.UsuarioRepository;
+import java.time.LocalDate;
 
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CampanaTest {
+    // entidades necesarias
     @Autowired
-    private CampanaRepository campanaRespository;
+    private CampanaController campanaController;
 
-    // Configuraciones
+    @Autowired
+    private RolRepository rolRepository;
 
-    private Campana campana = null;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private EntidadSaludRepository entidadRepository;
+
+    @Autowired
+    private LocalizacionRepository localizacionRepository;
+
+    // pre Configuraciones
+    private CampanaDTO campana = null;
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode permisos;
+    // crear un json para la prueba, comprobando una posible excepcion
+    {
+        try {
+            permisos = mapper.readTree("{\"crear\": true, \"editar\": false}");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error initializing permisos JSON", e);
+        }
+    }
+
+    Rol rol = new Rol(null, "administrador", "una descripcion", permisos);
+    Usuario usuario = new Usuario(null, TipoIdentificacion.CC, "124343", "pepito", "perez", "correo@me.com", "12345",
+            "12123123",
+            true, rol);
+    EntidadSalud entidad = new EntidadSalud(null, "una razon social", "calle falsa 123", "123444", 1L,
+            java.time.LocalDate.parse("2025-05-05"), Estado.ACTIVO, usuario);
+    Localizacion localizacion = new Localizacion(null, "Caldas", "Manizales", "una vereda", "una localidad", 0.0, 0.0);
 
     @Test
     @Transactional
     void testGuardarNuevaCampana() {
-        campana = new Campana();
 
-        System.out.println(campana);
-        Campana resultado = campanaRespository.save(campana);
-
-        assertNotNull(resultado);
     }
-
 }
