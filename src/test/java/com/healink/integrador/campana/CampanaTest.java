109bbcaf1bee79846921ec997daba9e17872450a
camian <camian6@outlook.com>
2025-05-08 11:43:52 -0500

    feat: cambios en las configuraciones de las prueba
    se cambiaron las configuraciones iniciales de la prueba, tambien el como
    se corren antes de hacerlas, para dejarlas como al comienzo de las
    mismas y se agregó la prueba sin localizacion, todavía sin logica

--- src/test/java/com/healink/integrador/campana/CampanaTest.java
+++ src/test/java/com/healink/integrador/campana/CampanaTest.java
@@ -3,16 +3,26 @@ package com.healink.integrador.campana;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.healink.integrador.domain.campana.CampanaController;
 import com.healink.integrador.domain.campana.CampanaDTO;
 import com.healink.integrador.domain.campana.EstadoCampana;
@@ -26,12 +36,17 @@ import com.healink.integrador.domain.usuario.Estado;
 import com.healink.integrador.domain.usuario.TipoIdentificacion;
 import com.healink.integrador.domain.usuario.Usuario;
 import com.healink.integrador.domain.usuario.UsuarioRepository;
+import com.healink.integrador.security.ControladorAuth;
+import com.healink.integrador.security.SolicitudAcceso;
+
 import java.time.LocalDate;
 
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
 class CampanaTest {
     // entidades necesarias
     @Autowired
@@ -49,35 +64,51 @@ class CampanaTest {
     @Autowired
     private LocalizacionRepository localizacionRepository;
 
-    // pre Configuraciones
-    private CampanaDTO campana = null;
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode permisos;
-    // crear un json para la prueba, comprobando una posible excepcion
-    {
-        try {
-            permisos = mapper.readTree("{\"crear\": true, \"editar\": false}");
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error initializing permisos JSON", e);
+    Rol rol = null;
+    Usuario usuario = null;
+    EntidadSalud entidad = null;
+    Localizacion localizacion = null;
+    CampanaDTO campana = null;
+    ObjectMapper mapper = null;
+
+    // Este método se ejecutará antes de cada test
+    @BeforeEach
+    void setup() {
+        // Eliminar todas las entidades previas si es necesario
+        rolRepository.deleteAll();
+        usuarioRepository.deleteAll();
+        entidadRepository.deleteAll();
+        localizacionRepository.deleteAll();
+
+        // pre Configuraciones
+        mapper = new ObjectMapper();
+        JsonNode permisos;
+        // crear un json para la prueba, comprobando una posible excepcion
+        {
+            try {
+                permisos = mapper.readTree("{\"crear\": true, \"editar\": false}");
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Error initializing permisos JSON", e);
+            }
         }
+
+        rol = new Rol(null, "administrador", "una descripcion", permisos);
+        usuario = new Usuario(
+                null, TipoIdentificacion.CC, "124343",
+                "pepito", "perez", "correo@me.com", "12345",
+                "12123123",
+                Estado.ACTIVO, rol, 1L);
+        entidad = new EntidadSalud(
+                null, "una razon social",
+                1L,
+                usuario);
+        localizacion = new Localizacion(
+                null, "Caldas", "Manizales",
+                "una vereda", "una localidad", 0.0, 0.0);
+
     }
 
-    Rol rol = new Rol(null, "administrador", "una descripcion", permisos);
-    Usuario usuario = new Usuario(
-            null, TipoIdentificacion.CC, "124343",
-            "pepito", "perez", "correo@me.com", "12345",
-            "12123123",
-            Estado.ACTIVO, rol, 1L);
-    EntidadSalud entidad = new EntidadSalud(
-            null, "una razon social",
-            1L,
-            usuario);
-    Localizacion localizacion = new Localizacion(
-            null, "Caldas", "Manizales",
-            "una vereda", "una localidad", 0.0, 0.0);
-
     @Test
-    @Transactional
     void guardarNuevaCampanaTest() {
         rolRepository.save(rol);
         usuarioRepository.save(usuario);
@@ -104,5 +135,10 @@ class CampanaTest {
         assertNotNull(res);
         assertEquals(HttpStatus.OK, expectedResponse.getStatusCode());
         assertEquals(res.getBody(), expectedResponse.getBody());
+    }
+
+    @Test
+    void guardarCampanaSinLocalizacionTest() {
+
     }
 }
