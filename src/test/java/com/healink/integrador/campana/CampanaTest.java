e6e882edab0f077a802669db48c43ebfe25c6a6f
camian <camian6@outlook.com>
2025-05-08 18:10:43 -0500

    feat: test de agregar una campaña sin localizacion
    se agrega el test de una campaña sin localizacion, comenzando se agrega
    una campaña vacía, y que en consecuencia debe Fallar.
    Para este test, se debe de hacer toda la peticion al endpoint del api,
    para valodar los campos que se envían, y se agrega las librerias
    pertinentes, tambien se debe "iniciar sesion" para poder conectarnos al
    api, esas configuraciones se agregan al test, y a la configuracion de la
    clase campaña testt

--- src/test/java/com/healink/integrador/campana/CampanaTest.java
+++ src/test/java/com/healink/integrador/campana/CampanaTest.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -63,6 +65,18 @@ class CampanaTest {
 
     @Autowired
     private LocalizacionRepository localizacionRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ControladorAuth controlAuth;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     Rol rol = null;
     Usuario usuario = null;
@@ -158,6 +172,50 @@ class CampanaTest {
         assertNotNull(res);
         assertEquals(HttpStatus.OK, expectedResponse.getStatusCode());
         assertEquals(res.getBody(), expectedResponse.getBody());
+    }
+
+    @Test
+    void guardarCampanaSinLocalizacionTest() {
+        System.out.println("\n Segundo test");
+
+        campana = new CampanaDTO();
+
+        // guardar hash clave
+        usuario.setClave(passwordEncoder.encode(usuario.getClave()));
+        try {
+            // Crear un objeto de solicitud de acceso con los datos proporcionados
+            SolicitudAcceso solicitudAcceso = new SolicitudAcceso(TipoIdentificacion.CC, USUARIO, PASSWORD);
+            ResponseEntity<?> res = controlAuth.login(solicitudAcceso);
+            String token = "";
+            if (res.getStatusCode().value() == 200) {
+                // Obtener el body como objeto genérico
+                Object body = res.getBody();
+                // Convertirlo a JSON
+                JsonNode json = mapper.convertValue(body, JsonNode.class);
+
+                // Acceder al atributo "token"
+                token = json.get("token").asText();
+
+                MvcResult result = mockMvc.perform(post("/api/campana")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(campana)))
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isBadRequest()) // Verifica código de estado es 400 (Bad Request)
+                        .andExpect(jsonPath("$.validationErrors.localizacionId")
+                                .value("La campaña debe tener una localizacion"))
+                        .andReturn(); // Verifica el mensaje de error en
+                                      // 'localizacionId'
+                String responseBody = result.getResponse().getContentAsString();
+                System.out.println("📦 JSON Response: " + responseBody);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+            System.out.println(e);
+        }
     }
 
 }
