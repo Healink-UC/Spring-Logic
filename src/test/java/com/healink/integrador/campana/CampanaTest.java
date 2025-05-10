619e797c3c561a3176eff88d5511f47c36012871
camian <camian6@outlook.com>
2025-05-10 17:15:02 -0500

    feat: comentario de clases válidas en inválidas

--- src/test/java/com/healink/integrador/campana/CampanaTest.java
+++ src/test/java/com/healink/integrador/campana/CampanaTest.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -140,6 +139,8 @@ class CampanaTest {
         nuevaLocalizacion = localizacionRepository.save(localizacion);
 
     }
+
+    // clases válidas
 
     @Test
     void guardarNuevaCampanaTest() {
