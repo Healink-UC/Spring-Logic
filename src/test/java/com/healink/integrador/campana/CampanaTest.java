60376d2dab33977364c9c684fc270950f182c612
camian <camian6@outlook.com>
2025-06-08 18:30:28 -0500

    feat: configuracion cambiada
    se cambio la configuracion inicial de los test ya que las entidad
    usuarios y entidad salud cambiaron

--- src/test/java/com/healink/integrador/campana/CampanaTest.java
+++ src/test/java/com/healink/integrador/campana/CampanaTest.java
@@ -123,11 +123,12 @@ class CampanaTest {
                 null, TipoIdentificacion.CC, USUARIO,
                 "pepito", "perez", "correo@me.com", PASSWORD,
                 "12123123",
-                Estado.ACTIVO, rol, 1L);
+                Estado.ACTIVO, rol, 1L, null, null);
+
         entidad = new EntidadSalud(
                 null, "una razon social",
-                1L,
-                usuario);
+                null);
+
         localizacion = new Localizacion(
                 null, "Antioquia", "Medellin",
                 "San Antonio", "Centro", 0.0, 0.0);
@@ -137,7 +138,7 @@ class CampanaTest {
         usuario.setRol(nuevoRol);
 
         nuevoUsuario = usuarioRepository.save(usuario);
-        entidad.setUsuarioId(nuevoUsuario.getId());
+        entidad.setUsuarios(null);
 
         nuevaEntidad = entidadRepository.save(entidad);
         nuevaLocalizacion = localizacionRepository.save(localizacion);
