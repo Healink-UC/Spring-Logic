aeee61ec2d6a17a2eb1bcb97a6a25c01f1321e1d
Over.HCV <over.castrillon22396@ucaldas.edu.co>
2025-06-08 23:35:34 -0500

    feat: Addition again of roles to use in platform

---
+++ src/main/resources/db/migration/V1_0_0__populate_roles.sql
@@ -0,0 +1,11 @@
+
+-- Agregar TODOS los roles nuevos (administrador, desarrollador, entidad_salud, medico, auxiliar, paciente, embajador)
+INSERT INTO roles (id, nombre, descripcion, creado_por, fecha_creacion, permisos)
+VALUES 
+(1, 'administrador', 'Administrador del sistema', 'administrador', CURRENT_TIMESTAMP, '{"escritura":true, "lectura":true, "actualizacion":true, "eliminacion":true}'),
+(2, 'desarrollador', 'Desarrollador técnico del aplicativo', 'administrador', CURRENT_TIMESTAMP, '{"escritura":true, "lectura":true, "actualizacion":true, "eliminacion":true}'),
+(3, 'entidad_salud', 'Representante de entidad de salud', 'administrador', CURRENT_TIMESTAMP, '{"escritura":true, "lectura":true, "actualizacion":true, "eliminacion":true}'),
+(4, 'medico', 'Profesional médico de la salud', 'administrador', CURRENT_TIMESTAMP, '{"escritura":true, "lectura":true, "actualizacion":true, "eliminacion":true}'),
+(5, 'auxiliar', 'Personal auxiliar médico', 'administrador', CURRENT_TIMESTAMP, '{"escritura":true, "lectura":true, "actualizacion":true, "eliminacion":true}'),
+(6, 'paciente', 'Paciente o usuario de servicios', 'administrador', CURRENT_TIMESTAMP, '{"escritura":true, "lectura":true, "actualizacion":true, "eliminacion":true}'),
+(7, 'embajador', 'Embajador comunitario', 'administrador', CURRENT_TIMESTAMP, '{}');
