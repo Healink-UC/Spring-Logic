663b7d83de1fb80e217da2fa83189452c72bb25d
camian <camian6@outlook.com>
2025-06-10 17:03:42 -0500

    feat:agregar varios factores de riesgo campaña
    se agregó la funcion para adicionar un arreglo de factores de riesgo a
    una campaña, se agregó el endpoint, el servicio y el repositorio

--- src/main/java/com/healink/integrador/domain/campana_factor/CampanaFactoresService.java
+++ src/main/java/com/healink/integrador/domain/campana_factor/CampanaFactoresService.java
@@ -5,12 +5,22 @@ import org.springframework.stereotype.Service;
 import com.healink.integrador.core.service.ServicioGenerico;
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class CampanaFactoresService extends ServicioGenerico<CampanaFactores> {
 
+    private final CampanaFactoresRepository campanaFactoresRepository;
+
     public CampanaFactoresService(CampanaFactoresRepository campanaFactoresRepository) {
         super(campanaFactoresRepository);
+        this.campanaFactoresRepository = campanaFactoresRepository;
+    }
+
+    @Transactional
+    public List<CampanaFactores> agregarFactoresCampana(Long campanaId, List<Long> factoresIds) {
+        return this.campanaFactoresRepository.saveAllFactoresCampana(campanaId, factoresIds);
     }
 
 }
