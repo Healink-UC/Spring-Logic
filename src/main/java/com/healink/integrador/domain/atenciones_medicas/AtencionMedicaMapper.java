fc1b1de6f0504726f0d8b49ed0f73890d18f0dc7
Over.HCV <over.castrillon22396@ucaldas.edu.co>
2025-06-12 01:31:28 -0500

    semi-fix: Se mergeó develop con rama local

--- src/main/java/com/healink/integrador/domain/atenciones_medicas/AtencionMedicaMapper.java
+++
@@ -1,9 +0,0 @@
-// package com.healink.integrador.domain.atenciones_medicas;
-
-// import org.mapstruct.Mapper;
-
-// import com.healink.integrador.core.mapper.MapeadorGenerico;
-
-// @Mapper(componentModel = "spring")
-// public interface AtencionMedicaMapper extends MapeadorGenerico<AtencionMedica, AtencionMedicaDTO> {
-// }
--- src/main/java/com/healink/integrador/domain/atenciones_medicas/EstadoAtencionMedica.java
+++
@@ -1,6 +0,0 @@
-// package com.healink.integrador.domain.atenciones_medicas;
-
-// public enum EstadoAtencionMedica {
-//     EN_PROCESO,
-//     COMPLETADA,
-// }
